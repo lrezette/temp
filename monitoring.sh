cf2dbc39153feb78070deb848bc2631bf5ec1fc7
Livio <55594647+lrezette@users.noreply.github.com>
2025-06-28 15:25:06 +0200

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,43 +1,42 @@
-#! /bin/bash
+#!/bin/sh
 
 arch=$(uname -a)
 
-cpu=$(grep "physical id" /proc/cpuinfo | wc -l)
-
-vcpu=$(grep -c processor /proc/cpuinfo)
+cpu=$(grep "physical id" /proc/cpuinfo | sort -u | wc -l)
+vcpu=$(grep -c ^processor /proc/cpuinfo)
 
 ram_total=$(free --mega | awk '$1 == "Mem:" {print $2}')
-ram_free=$(free --mega | awk '$1 == "Mem:" {print $3}')
+ram_used=$(free --mega | awk '$1 == "Mem:" {print $3}')
 ram_percent=$(free --mega | awk '$1 == "Mem:" {printf("%.2f"), $3/$2*100}')
 
-disk_total=$(df -BG / | awk 'NR==2 {print substr($2, 1, length($2)-1) "Go"}')
+disk_total=$(df -BG / | awk 'NR==2 {gsub("G", "", $2); print $2}')
 disk_used=$(df -m / | awk 'NR==2 {print $3}')
 disk_percent=$(df / | awk 'NR==2 {print $5}')
 
-cpu_percent=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8"%"}')
+cpu_percent=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8}' | awk '{printf("%.1f", $1)}')
 
-last_reboot=$(who -b | awk '$1 == "system" {print $3 " " $4}')
+last_reboot=$(who -b | awk '{print $3, $4}')
 
-lvm_check=$(if sudo lvdisplay &> /dev/null; then echo "yes"; else echo "no"; fi)
+lvm_check=$(lsblk | grep -q "lvm" && echo "yes" || echo "no")
 
 tcp=$(ss -ta | grep ESTAB | wc -l)
 
 user_log=$(users | wc -w)
 
-ip=$(hostname -I)
-mac=$(ip link show | awk '/ether/ {print $2}')
+ip=$(hostname -I | awk '{print $1}')
+mac=$(ip link show | awk '/ether/ {print $2}' | head -n 1)
 
-sudo=$(history | grep -c 'sudo')
+sudo=$(journalctl _COMM=sudo 2>/dev/null | grep -c COMMAND)
 
-wall "	Architecture: $arch
-	CPU physical: $cpuf
-	vCPU: $cpuv
-	Memory Usage: $ram_use/${ram_total}MB ($ram_percent%)
-	Disk Usage: $disk_use/${disk_total} ($disk_percent%)
-	CPU load: $cpu_fin%
-	Last boot: $lb
-	LVM use: $lvmu
-	Connections TCP: $tcpc ESTABLISHED
-	User log: $ulog
-	Network: IP $ip ($mac)
-	Sudo: $cmnd cmd"
+wall "#Architecture: $arch
+#CPU physical : $cpu
+#vCPU : $vcpu
+#Memory Usage: ${ram_used}/${ram_total}MB (${ram_percent}%)
+#Disk Usage: ${disk_used}/${disk_total}Gb (${disk_percent})
+#CPU load: ${cpu_percent}%
+#Last boot: $last_reboot
+#LVM use: $lvm_check
+#Connexions TCP : $tcp ESTABLISHED
+#User log: $user_log
+#Network: IP $ip ($mac)
+#Sudo : $sudo cmd"
